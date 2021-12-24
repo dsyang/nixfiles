a1f7d37b754e784851a516442eb62c5b6e05f9fe
Dan Yang <dsyang92@gmail.com>
2021-12-24 00:44:01 -0800

    ported gitconfig raw config

--- home.nix
+++ home.nix
@@ -10,6 +10,9 @@
     pkgs.tmux
   ];
 
+  # Raw configuration files
+  home.file.".gitconfig".source = ./osx/gitconfig;
+
   # This value determines the Home Manager release that your
   # configuration is compatible with. This helps avoid breakage
   # when a new Home Manager release introduces backwards
