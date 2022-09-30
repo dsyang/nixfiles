b0e1e6c4c3a765a7616bf1c2ce80d796d003076d
Dan Yang <dan@makenotion.com>
2022-09-30 12:51:09 -0700

    nolint on push

--- zshrc-snippets/notion.zsh
+++ zshrc-snippets/notion.zsh
@@ -1,3 +1,5 @@
 eval "$(notion completion --install)"
 
-alias -g lintcommit="./gradlew ktlintformat && git commit -a --allow-empty -m 'ran ktlintformat'"
+alias -g lintcommit="./gradlew formatkotlin && git commit -a --allow-empty -m 'ran formatkotlin'"
+
+export NOTION_NO_PREPUSH=true
